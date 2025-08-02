19d0fd937d2507e819186fe9aef4a9c9600bae5a
Bhumika-Meena <159446169+Bhumika-Meena@users.noreply.github.com>
2025-08-02 20:09:09 +0530

    Update api.js

--- lms-frontend/src/services/api.js
+++ lms-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://lead-management-system-vzgu.onrender.com';
+const API_BASE_URL = 'https://lead-management-system-3z38.onrender.com';
 
 // Create axios instance
 const api = axios.create({
